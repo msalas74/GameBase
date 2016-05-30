b6416a517e25a436417c3b31dc58a95615947a57
Matias Salas <mat.salas@comcast.net>
2016-05-30 13:13:08 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Base Game Starter
+
+A game framework
+
+## Getting Started
+
+
+## Built With
+
+* Phaser - The game Framework
+* Firebase - For the data stuff
+* Vanilla JS - For the good stuff
+* Gulp - To automate my build and stuff
+* NodeJS - For the server side stuff
+* NPM - To install all the dependencies...and stuff
+* Sublime Text - Where it's all put together!
+* Code Style - [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+ 
+
+## Authors
+
+* **Matias Salas** - *Initial work* - [Website Portfolio](http://www.matiasis.me)
+
+## License
+
+This project is licensed under the ISC License - see the [LICENSE.md](LICENSE.md) file for details
+
---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+var gulp = require('gulp')
+var gutil = require('gulp-util')
+var jshint = require('gulp-jshint')
+var sass = require('gulp-ruby-sass')
+var sourcemaps = require('gulp-sourcemaps')
+var webserver = require('gulp-webserver')
+var imagemin = require('gulp-imagemin')
+var imageminJpegtran = require('imagemin-jpegtran')
+var uglify = require('gulp-uglify')
+var minifyHTML = require('gulp-minify-html')
+var concat = require('gulp-concat')
+
+// environment
+var environment = 'development'
+
+var jsFiles = [
+  'app/js/app.js'
+]
+
+gulp.task('js', function () {
+  return gulp.src(jsFiles)
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'))
+})
+
+gulp.task('data', function () {
+  return gulp.src('app/js/*.json')
+    .pipe(gulp.dest('build/js'))
+})
+
+// JS linting task
+gulp.task('jshint', function () {
+  return gulp.src('app/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+})
+
+
+// Styles build task and concat all the files
+gulp.task('styles', function () {
+  return gulp.src(['app/css/*.css'])
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('build/css'))
+})
+
+// Watch task
+gulp.task('watch', function () {
+  gulp.watch('app/js/*.js', ['jshint', 'js'])
+  gulp.watch('app/*.html', ['html'])
+  gulp.watch('app/css/styles.css', ['styles'])
+})
+
+// Minify task
+gulp.task('html', function () {
+  if (environment === 'production') {
+    return gulp.src('app/index.html')
+      .pipe(minifyHTML())
+      .pipe(gulp.dest('build/'))
+  } else {
+    return gulp.src('app/index.html')
+      .pipe(gulp.dest('build/'))
+  }
+})
+
+gulp.task('views', function () {
+  if (environment === 'production') {
+    return gulp.src('app/views/*.html')
+      .pipe(minifyHTML())
+      .pipe(gulp.dest('build/views/'))
+  } else {
+    return gulp.src('app/views/*.html')
+      .pipe(gulp.dest('build/views/'))
+  }
+})
+
+// image optimize task =====================
+gulp.task('images', function () {
+  return gulp.src(['app/images/*', 'app/images/**/*'])
+    // .pipe(imagemin({
+    //  progressive: true,
+    //  use:[imageminJpegtran({ptogressive: true})]
+    // }))
+    .pipe(gulp.dest('build/images'))
+})
+
+// fonts
+gulp.task('fonts', function () {
+  gulp.src('app/fonts/*')
+    .pipe(gulp.dest('build/fonts'))
+})
+
+// Webserver
+gulp.task('webserver', function () {
+  gulp.src('build/')
+    .pipe(webserver({
+      livereload: true,
+      open: true
+    }))
+})
+// Build task
+if (environment !== 'production') {
+  gulp.task('build', ['jshint', 'html', 'js', 'styles', 'fonts', 'images'])
+} else {
+  gulp.task('build', ['jshint', 'html', 'js', 'fonts', 'images'])
+}
+// Default task
+gulp.task('default', ['jshint', 'styles', 'watch', 'webserver'])
